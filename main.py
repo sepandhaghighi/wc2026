c7a7feed9e7bf306dd36b27eb60aeba1d5763df6
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-06-13 10:08:59 +0330

    Match 05 (#10)
    * fix : update game data
    
    * data : add m05
    
    * data : add missed m05 data

--- main.py
+++ main.py
@@ -474,11 +474,11 @@ def save_match_prediction_and_session(
 
 if __name__ == "__main__":
     current_phase = Phase.GROUP.value 
-    raw_match_id = "WC2026-M04"
+    raw_match_id = "WC2026-M05"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.USA.value
-    country_b = Team.PARAGUAY.value
+    country_a = Team.QATAR.value
+    country_b = Team.SWITZERLAND.value
     match_host = Host.USA.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
