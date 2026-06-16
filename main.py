496bce7627a2a3661c60320bc9fcf5f8b806d72c
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-06-16 10:35:23 +0330

    Match 18 (Iraq - Norway) (#27)
    * fix : update match params
    
    * data : add m18 data

--- main.py
+++ main.py
@@ -485,11 +485,11 @@ def save_match_prediction_and_session(
 
 if __name__ == "__main__":
     current_phase = Phase.GROUP.value 
-    raw_match_id = "WC2026-M17"
+    raw_match_id = "WC2026-M18"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.FRANCE.value
-    country_b = Team.SENEGAL.value
+    country_a = Team.IRAQ.value
+    country_b = Team.NORWAY.value
     match_host = Host.USA.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
