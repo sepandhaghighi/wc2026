a6784ee48dffc49a68a3710ab844c8898a0f26e4
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-06-18 03:48:59 +0330

    Match 23 (Ghana - Panama) (#34)
    * fix : update match params
    
    * data : add m23 data
    
    * data : update with host=Canada

--- main.py
+++ main.py
@@ -488,12 +488,12 @@ def save_match_prediction_and_session(
 
 if __name__ == "__main__":
     current_phase = Phase.GROUP.value 
-    raw_match_id = "WC2026-M22"
+    raw_match_id = "WC2026-M23"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.ENGLAND.value
-    country_b = Team.CROATIA.value
-    match_host = Host.USA.value
+    country_a = Team.GHANA.value
+    country_b = Team.PANAMA.value
+    match_host = Host.CANADA.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
     team_b_metrics = fetch_true_national_team_form(international_results_df, country_b, last_n=15)
