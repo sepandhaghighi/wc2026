36ea34386c5071226148a7d338ae88f033f8124d
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-06-20 09:49:54 +0330

    fix : update match params

--- main.py
+++ main.py
@@ -491,11 +491,11 @@ def save_match_prediction_and_session(
 
 if __name__ == "__main__":
     current_phase = Phase.GROUP.value 
-    raw_match_id = "WC2026-M32"
+    raw_match_id = "WC2026-M33"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.TURKEY.value
-    country_b = Team.PARAGUAY.value
+    country_a = Team.NETHERLANDS.value
+    country_b = Team.SWEDEN.value
     match_host = Host.USA.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
