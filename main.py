7b499f8ad81e9ef68e6d261acc5ec59cfdbcdcb1
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-06-21 10:39:19 +0330

    Match 43 (Norway - Senegal) (#56)
    * fix : update match params
    
    * data : add m43 data

--- main.py
+++ main.py
@@ -491,11 +491,11 @@ def save_match_prediction_and_session(
 
 if __name__ == "__main__":
     current_phase = Phase.GROUP.value 
-    raw_match_id = "WC2026-M42"
+    raw_match_id = "WC2026-M43"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.FRANCE.value
-    country_b = Team.IRAQ.value
+    country_a = Team.NORWAY.value
+    country_b = Team.SENEGAL.value
     match_host = Host.USA.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
