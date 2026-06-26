725ffaa037a884e9ce1aa7b60a3e2f1dbd674e07
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-06-26 14:24:28 +0330

    Match 63 (Cape Verde - Saudi Arabia) (#79)
    * fix : update match params
    
    * data : add m63 data

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from utils import *
 
 if __name__ == "__main__":
     current_phase = Phase.GROUP.value 
-    raw_match_id = "WC2026-M62"
+    raw_match_id = "WC2026-M63"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.SENEGAL.value
-    country_b = Team.IRAQ.value
-    match_host = Host.CANADA.value
+    country_a = Team.CAPE_VERDE.value
+    country_b = Team.SAUDI_ARABIA.value
+    match_host = Host.USA.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
     team_b_metrics = fetch_true_national_team_form(international_results_df, country_b, last_n=15)
