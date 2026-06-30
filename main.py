9486ae496f92aed8ae2bf7a3cfaddace710d9c1b
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-06-30 17:35:24 +0330

    Match 79 (Mexico - Ecuador) (#101)
    * fix : update match params
    
    * data : add m79 data

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from utils import *
 
 if __name__ == "__main__":
     current_phase = Phase.KNOCKOUT.value 
-    raw_match_id = "WC2026-M78"
+    raw_match_id = "WC2026-M79"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.FRANCE.value
-    country_b = Team.SWEDEN.value
-    match_host = Host.USA.value
+    country_a = Team.MEXICO.value
+    country_b = Team.ECUADOR.value
+    match_host = Host.MEXICO.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
     team_b_metrics = fetch_true_national_team_form(international_results_df, country_b, last_n=15)
