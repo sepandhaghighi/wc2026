aadc6b63e423e721aea39346dc2fffa1526edac4
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-07-07 17:57:57 +0330

    Match 95 (Argentina - Egypt) (#123)
    * fix : update match params
    
    * data : add m95 data

--- src/main.py
+++ src/main.py
@@ -7,11 +7,11 @@ from utils import *
 
 if __name__ == "__main__":
     current_phase = Phase.KNOCKOUT.value 
-    raw_match_id = "WC2026-M94"
+    raw_match_id = "WC2026-M95"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.USA.value
-    country_b = Team.BELGIUM.value
+    country_a = Team.ARGENTINA.value
+    country_b = Team.EGYPT.value
     match_host = Host.USA.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
