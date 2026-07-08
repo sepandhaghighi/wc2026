726843eeea610d52fcb2d90d3021e6c924c2713d
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-07-08 17:19:36 +0330

    fix : update match params

--- src/main.py
+++ src/main.py
@@ -7,11 +7,11 @@ from utils import *
 
 if __name__ == "__main__":
     current_phase = Phase.KNOCKOUT.value 
-    raw_match_id = "WC2026-M97"
+    raw_match_id = "WC2026-M98"
     match_id = raw_match_id.replace("WC2026-", "") if "WC2026-" in raw_match_id else raw_match_id
     
-    country_a = Team.FRANCE.value
-    country_b = Team.MOROCCO.value
+    country_a = Team.SPAIN.value
+    country_b = Team.BELGIUM.value
     match_host = Host.USA.value
     international_results_df = pd.read_csv(HISTORICAL_RESULTS_URL)
     team_a_metrics = fetch_true_national_team_form(international_results_df, country_a, last_n=15)
